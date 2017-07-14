4234aa7b82c8addae7757c525f9415e00cce16b9
Kristine <kristine@7mind.de>
2017-07-14 21:31:37 +0200

    myday changes

--- app/models/my_day.rb
+++ app/models/my_day.rb
@@ -2,4 +2,5 @@ class MyDay < ApplicationRecord
   has_many :tasks
   validates :reflection, presence: true,
                     length: { minimum: 5 }
+  validates :today, presence: true
 end
--- db/migrate/20170707190251_create_my_days.rb
+++ db/migrate/20170707190251_create_my_days.rb
@@ -3,7 +3,6 @@ class CreateMyDays < ActiveRecord::Migration[5.1]
     create_table :my_days do |t|
       t.text :reflection
       t.date :today
-
       t.timestamps
     end
   end
