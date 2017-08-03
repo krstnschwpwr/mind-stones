2a020d5c181f59926cccf7740f57350723cc985b
Kristine <kristine@7mind.de>
2017-08-03 17:17:55 +0200

    testing user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,23 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(username: "Halliga", password: "foobar", password_confirmation: "foobar")
+    @user = users(:valid)
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
+  test 'invalid without name' do
+    @user.username = nil
+    refute @user.valid?, 'saved user without a name'
+    assert_not_nil @user.errors[:username], 'no validation error for name present'
+
+  end
+
+  test 'invalid password' do
+    user = User.new(username: 'Halliga', password_digest: 'foobar', password_confirmation: 'foobar3')
+    assert user.password_digest = true
+  end
 
 end
