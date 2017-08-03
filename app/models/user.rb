b46d4447b6758b6ef3221eb8497f24b77b796c30
Kristine <kristine@7mind.de>
2017-08-03 16:05:46 +0200

    wrong name for password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks
 
-  attr_accessor  :username, :password #für sessions? :username, :password, :password_confirmation
+  attr_accessor  :username, :password_digest #für sessions? :username, :password, :password_confirmation
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 6..20, :on => :create
